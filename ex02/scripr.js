4dc580dac1737099d870f55ee03338c860a46304
GersonESS <gebhsantos@gmail.com>
2025-02-01 05:39:45 -0300

    a

--- ex02/scripr.js
+++ ex02/scripr.js
@@ -1,12 +1,28 @@
-// script.js
+function adicionarTarefa() {
 
-// Adiciona um evento de clique ao link "Contato"
-document.getElementById('linkContato').addEventListener('click', function(event) {
-    event.preventDefault(); // Impede o comportamento padrão do link
-    const contatoSection = document.getElementById('contato');
-    if (contatoSection.style.display === 'none' || contatoSection.style.display === '') {
-        contatoSection.style.display = 'block'; // Mostra a seção
+    //recebe valor do input do usuário
+    const inputTarefa = document.getElementById("inputTarefa")
+    let tarefa = inputTarefa.value.trim()
+
+    const mensagem = document.getElementById("mensagem")
+
+    // se o valor do input for vazio então mostre uma mensagem de erro para o usuário
+    if (tarefa == "") {
+        //mostre uma mensagem de erro
+        let mensagemErro = "Digite uma tarefa para adicioná-la a sua lista!"
+        mensagem.textContent = mensagemErro
     } else {
-        contatoSection.style.display = 'none'; // Oculta a seção
+        //mensagem de tarefa adicionada com sucesso
+        let mensagemSucesso = "Tarefa adicionada com sucesso!"
+        mensagem.textContent = mensagemSucesso
+
+        //cria novo item (li) e insere na (lista ul)
+        const listaTarefas = document.getElementById("listaTarefas")
+        let novaTarefa = document.createElement("li")
+        novaTarefa.textContent = tarefa
+        listaTarefas.appendChild(novaTarefa)
     }
-});
+
+    //limpa o input do usuário
+    inputTarefa.value = ""
+}
